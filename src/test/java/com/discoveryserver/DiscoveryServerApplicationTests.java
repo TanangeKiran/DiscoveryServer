8c1f4d4590633671c4bf56b811a6fbcf75d40b5f
kiran tanange <kirantanange@gmail.com>
2023-08-22 09:49:14 +0530

    Discovery server commit

---
+++ src/test/java/com/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discoveryserver;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class DiscoveryServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
